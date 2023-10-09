1f38232646bb6cfb17bac5672df35992e4a58016
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2023-10-09 11:08:04 +0600

    reduct setup done

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView>
+
+            <Text>HomeScreen</Text>
+
+        </SafeAreaView>
+
+    )
+}
